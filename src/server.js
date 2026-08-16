6b6c4ed5427f0fe04f3bd629ce3bb049f8498e32
track2000one <track2000one@gmail.com>
2026-08-16 19:22:15 +0300

    feat: initialize organization structure

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import { app } from './app.js';
 import { ensureBootstrapAdmin } from './bootstrapAdmin.js';
 import { ensureAccountingTransformationBaseline } from './services/accountingCycles.service.js';
+import { ensureOrganizationStorage } from './services/organization.service.js';
 
 const port = Number(process.env.PORT || 8080);
 
 const startServer = async () => {
   await ensureBootstrapAdmin();
+  await ensureOrganizationStorage();
   await ensureAccountingTransformationBaseline();
 
   app.listen(port, () => {
